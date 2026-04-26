3ff755ed524f4b2f701a8ad456b058c0752c9790
Muthukannan Baskaran <muthukannanbaskaran@gmail.com>
2026-04-26 08:37:34 +0530

    moving the context to global main.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,10 @@
 import './App.css';
-import { UserContextProvider } from './context/UserContext';
 import ChangeThemeButton from './components/ChangeThemeButton';
-import { LanguageContextProvider } from './context/LanguageContext';
 
 export default function App() {
   return (
     <div className="App">
-      <LanguageContextProvider>
-        <UserContextProvider>
-          <ChangeThemeButton />
-        </UserContextProvider>
-      </LanguageContextProvider>
+      <ChangeThemeButton />
     </div>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { LanguageContextProvider } from './context/LanguageContext';
+import { UserContextProvider } from './context/UserContext';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <LanguageContextProvider>
+      <UserContextProvider>
+        <App />
+      </UserContextProvider>
+    </LanguageContextProvider>
   </StrictMode>,
 )
